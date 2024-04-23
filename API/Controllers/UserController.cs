72592e20946f993e457ca3398c463187a76cf254
Jason Fung <x00174641@mytudublin.ie>
2024-04-24 00:48:16 +0100

    Added new route to fetch userId for app.

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using CryptoApi.Models;
 using CryptoApi.Repositories;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace CryptoApi.Controllers
 {
@@ -49,7 +54,32 @@ namespace CryptoApi.Controllers
         {
             return "Hello World";
         }
+        
+        [HttpGet("fetchId")]
+        [Authorize] // Add [Authorize] attribute to allow only authenticated users to access this endpoint
+        public async Task<ActionResult<string>> fetchId()
+        {
+            try
+            {
+                // Get the user's ID (primary key)
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
+                // Check if user ID is not null
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    // Return the user's ID
+                    return $"{userId}";
+                }
+
+                // If the user ID is null or empty, return a 404 Not Found response
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or return a generic error message
+                return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred while processing your request.");
+            }
+        }
 
         [HttpPost]
         public ActionResult<User> CreateUser(User user)
