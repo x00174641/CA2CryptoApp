872d1befbe4bd9b61972eee86e4b59f907a8f62c
Jason Fung <x00174641@mytudublin.ie>
2024-04-23 23:22:18 +0100

    Revert "Added sample mobile app, Added login route using identity API route."
    This reverts commit 04a90ac5aea7e452db0f474adb27319263611f1f.

--- App/components/ui/collapsible.tsx
+++
@@ -1,7 +0,0 @@
-import * as CollapsiblePrimitive from '@rnr/collapsible';
-const Collapsible = CollapsiblePrimitive.Root;
-const CollapsibleTrigger = CollapsiblePrimitive.Trigger;
-const CollapsibleContent = CollapsiblePrimitive.Content;
-export { Collapsible, CollapsibleTrigger, CollapsibleContent };
-
-// See file: [[packages/collapsible/src/index.ts]]
