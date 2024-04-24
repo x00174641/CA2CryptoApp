a9a9d8c529fe078ef0c21e5449f9dbd1a0b1bd24
Max Witalec <mwitalec01@gmail.com>
2024-04-24 21:02:06 +0100

    Added route to check current price of crypto

--- API/Controllers/CryptoTransactionController.cs
+++ API/Controllers/CryptoTransactionController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace CryptoApi.Controllers
 {
@@ -39,5 +40,43 @@ namespace CryptoApi.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred while processing your request.");
             }
         }
+        
+        [HttpGet("getCryptoPrice/{cryptoSymbol}")]
+        public async Task<decimal> GetCryptoPrice(string cryptoSymbol)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "cfa2caddadmshe13d9ac6b989363p10da2ejsn16d1e4a1d93f");
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "real-time-quotes1.p.rapidapi.com");
+
+                var requestUri = $"https://real-time-quotes1.p.rapidapi.com/api/v1/realtime/crypto?source={cryptoSymbol}&target=USD";
+                using (var response = await client.GetAsync(requestUri))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+            
+                    // Console.WriteLine the JSON response
+                    Console.WriteLine(body);
+
+                    // Parse the JSON response
+                    var cryptoDataArray = JArray.Parse(body);
+                    var cryptoData = cryptoDataArray.FirstOrDefault();
+
+                    if (cryptoData != null)
+                    {
+                        var price = cryptoData["price"].Value<decimal>();
+                        return price;
+                    }
+                    else
+                    {
+                        // Handle case where no data is returned
+                        throw new Exception("No data returned from API.");
+                    }
+                }
+            }
+        }
+
     }
+    
+    
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd42ecd980743ed8e60e27079cab73927c700cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc33d2345320e8c875810254584fb9c8beed7f03")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
